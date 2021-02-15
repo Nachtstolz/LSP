647f775bf045275300523d31d98d7b710bc0335b
csh <sayong4424@gmail.com>
2021-02-16 00:01:49 +0900

    210215

--- Backup/function.h
+++ Backup/function.h
@@ -7,6 +7,14 @@
 #include <pthread.h>
 #include <sys/type.h>
 #include <sys/stat.h>
+#include <ctype.h>
+
+typedef struct logDetail{
+	char time[16]; //일단 시간을 문자열로 저장하고자 했음
+	char name[256]; //백업해야할 파일 이름
+	char descript[32]; //수행 내용
+	struct logDetail* link;
+}LogDetail
 
 typedef struct node {
 	char route[256];
--- Backup/main.c
+++ Backup/main.c
@@ -1,6 +1,7 @@
 #include "function.h"
 
 Diclist* dic = NULL;
+FILE* logfile;
 
 Linklist* GetNode(){
 	Linklist* tmp;
@@ -16,14 +17,36 @@ Linklist* GetNode(){
 	return tmp;
 }*/
 
-Linkedlist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
+Linklist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
 	int idx = 0;
-	
+	FILE* fp;
+	struct stat f_info;
+	mode_t f_mode;
+	LogDetail* put_log;
+
 	if(strcmp(file, "\0") == 0){
 		puts("Fail to add command");
 		//basic 함수로 넘어가서 return 처리가 바로 될 수 있도록 조건 넣어줄 것
 		return head;
 	}
+	// 백업해야할 파일이 존재하지 않을 때 처리
+	if(fopen(file, "r") == NULL){
+		puts("Fail to add command");
+	}
+	// 백업해야할 파일이 일반파일이 아닐 경우 처리
+	int f_stat = stat(file, &f_info);
+	f_mode = f_info.st_mode;
+	if(!S_ISREG(f_mode)){
+		puts("Fail to open file");
+	}
+	// 백업해야할 파일이 백업리스트에 존재하는 지 확인 후 처리
+	
+	//PERIOD 처리
+	if(strcmp(option[0], '\0') == 0){ //PERIOD 입력 없을 시
+		puts("Fail to add command");
+	}
+	if(isdigit(atoi(option[0])) != 0){ //int, float, double 구분해야. 아직 X
+		puts("Fail to add command"); //PERIOD가 실수일 때
 	do{
 		if(dic[idx] == NULL) break; //특정 인덱스의 값이 없다면 break
 	}while(idx++)
@@ -36,6 +59,16 @@ Linkedlist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
 		head->route = file;
 		head->period = atoi(option[0]);
 		head->link = NULL;
+		
+		//log 파일 게시 작업은 훗날 따로 함수를 만들어서 지정 가능성도 열어놓기
+		put_log->time = 0; //로그 시간 설정
+		put_log->name = file;
+		put_log->descript = "added";
+		//link 부분도 지정해야
+
+		//logfile에 게시하는 작업
+		fputs(, logflie);
+
 		break;
 	}
 	Add(head->link, file, option);
@@ -43,6 +76,23 @@ Linkedlist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
 
 	return head;
 }
+
+Linklist* Remove(Linklist* head, char path[], char file[], char option[][32]){
+	//FILENAME 입력 없을 시
+	if(strcmp(file, "\0") == 0){
+		puts("Fail to add command");
+		//basic 함수로 넘어가서 return 처리가 바로 될 수 있도록 조건 넣어줄 것
+		return head;
+	}
+	//백업을 중단할 파일이 백업 리스트에 존재하지 않을 시(로그 파일)
+
+	
+
+
+
+	return head;
+}
+
 
 void base_print(Linklist* head, char path[256]){
 	char input[256];
@@ -111,6 +161,11 @@ int main(char argc, char *argv[]){
 	char path[256];
 	struct stat dir_info;
 	mode_t dir_mode;
+	//FILE* logfile;
+
+	if(logfile = fopen("./logfile", "a+") == NULL){ //a+ or ra로 읽기도 동시에 할 수 있는지..
+		logfile = fopen("./logfile", "w+");
+	}
 
 	if(strchr(argv, '/') == NULL){ //상대 경로의 경우 앞에 ./ 추가
 		char way1[8] = "./";
@@ -140,7 +195,7 @@ int main(char argc, char *argv[]){
 		return 0;
 	}
 	while(re){
-		base_print(head);
+		base_print(head,path);
 	}
 
 	return 0;
