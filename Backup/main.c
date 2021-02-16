20f6ef167c40156a6fa3b6295370edd47f997ad8
csh <sayong4424@gmail.com>
2021-02-16 23:37:29 +0900

    210216

--- Backup/main.c
+++ Backup/main.c
@@ -17,12 +17,77 @@ Linklist* GetNode(){
 	return tmp;
 }*/
 
-Linklist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
+LogDetail* GetLog(){
+	LogDetail* tmp;
+	tmp = (LogDetail*)malloc(sizeof(LogDetail));
+	tmp->link = NULL;
+	return tmp;
+}
+
+void Addlog(LogDetail* lhead, char file[]){ //로그파일에 로그 추가
+	
+	if(lhead == NULL){
+		lhead = GetLog();
+		lhead->time = 0; //시간 스레드 이용해야. 임의로 0 설정.
+		lhead->name = file;
+		lhead->descript = "added";
+		return;
+	}
+	Addlog(lhead->link, file);
+	// 로그 파일에 해당 연결리스트를 작성하는 것은 돌아가서 진행하는 걸로.
+}
+
+void Editlog(LogDetail* lhead){ //로그파일에 로그 수정
+
+}
+
+void Removelog(LogDetail* lhead, char file[]){ //로그파일에 로그 삭제
+	
+	if(lhead == NULL){
+		lhread = GetLog();
+		lhead->time = 0; //시간 스레드 이용해야. 임의로 0 설정.
+		lhead->name = file;
+		lhead->descript = "deleted";
+		return;
+	}
+	Removelog(lhead->link, file);
+}
+
+void Insertlog(LogDetail* lhead){
+	if(lhead == NULL){
+		return;
+	}
+	char new[512];
+	char n_time[32];
+	n_time[0] = "[";
+	strcat(n_time, lhead->time);
+	strcat(n_time, "]");
+	
+	strcat(new, n_time);
+	strcat(new, " ");
+	strcat(new, lhead->name);
+	strcat(new, " ");
+	strcat(new, lhead->descript);
+
+	fputs(new, logfile);
+	
+	Insertlog(lhead->link);
+}
+
+int CheckFile(LogDetail* lhead, char cm_file[]){
+	if(lhead == NULL){
+		return 1;
+	}
+	if(strcmp(lhead->file, cm_file) == 0) { return 0; }
+	CheckFile(lhead->link, cm_file);
+}
+
+Linklist* Add(Linkedlist* head, LogDetail* lhead, char path[], char file[], char option[][32]){
 	int idx = 0;
 	FILE* fp;
 	struct stat f_info;
 	mode_t f_mode;
-	LogDetail* put_log;
+	//LogDetail* put_log;
 
 	if(strcmp(file, "\0") == 0){
 		puts("Fail to add command");
@@ -40,13 +105,18 @@ Linklist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
 		puts("Fail to open file");
 	}
 	// 백업해야할 파일이 백업리스트에 존재하는 지 확인 후 처리
-	
+	if(CheckFile(lhead, file) == 1){
+		puts("Fail to open file");
+	}
+
 	//PERIOD 처리
 	if(strcmp(option[0], '\0') == 0){ //PERIOD 입력 없을 시
 		puts("Fail to add command");
 	}
-	if(isdigit(atoi(option[0])) != 0){ //int, float, double 구분해야. 아직 X
+	if(((float)atoi(option[0])) % 1.0 > 0){ 
 		puts("Fail to add command"); //PERIOD가 실수일 때
+	}
+
 	do{
 		if(dic[idx] == NULL) break; //특정 인덱스의 값이 없다면 break
 	}while(idx++)
@@ -60,41 +130,53 @@ Linklist* Add(Linkedlist* head, char path[], char file[], char option[][32]){
 		head->period = atoi(option[0]);
 		head->link = NULL;
 		
-		//log 파일 게시 작업은 훗날 따로 함수를 만들어서 지정 가능성도 열어놓기
-		put_log->time = 0; //로그 시간 설정
-		put_log->name = file;
-		put_log->descript = "added";
-		//link 부분도 지정해야
-
 		//logfile에 게시하는 작업
-		fputs(, logflie);
+		//함수로 넘겨서 진행할 것. 함수 명시해야.
+		Addlog(lhead, file);
+
+		//fputs(lhead, logflie);
+		// 연결리스트를 그대로 fputs하는 것이 불가능하기 때문에 넣는 함수를 임의로 만들어주는 방법 생각
+		Insertlog(lhead);
 
 		break;
 	}
-	Add(head->link, file, option);
+	Add(head->link, lhead, path, file, option);
 	
 
 	return head;
 }
 
-Linklist* Remove(Linklist* head, char path[], char file[], char option[][32]){
+Linklist* Remove(Linklist* head, LogDetail* lhead, char path[], char file[], char option[][32]){
 	//FILENAME 입력 없을 시
 	if(strcmp(file, "\0") == 0){
-		puts("Fail to add command");
+		puts("Fail to remove command");
 		//basic 함수로 넘어가서 return 처리가 바로 될 수 있도록 조건 넣어줄 것
 		return head;
 	}
 	//백업을 중단할 파일이 백업 리스트에 존재하지 않을 시(로그 파일)
-
+	if(CheckFile(lhead, file) == 1){
+		puts("Fail to remove command");
+	}
 	
+	if(strcmp(head->link->route, file) == 0){
+		//head->link = head->link->link;
+		Linklist* tmp = NULL;
+		tmp = head->link;
+		head = tmp->link;
+		free(tmp);
+		//잘 작동하는지 확인할 필요 있음.
 
+		Removelog(lhead, file);
+		Insertlog(lhead);
+	}
 
+	Remove(head->link, lhead, path, file, option);
 
 	return head;
 }
 
 
-void base_print(Linklist* head, char path[256]){
+void base_print(Linklist* head, char path[256], LogDetail* lhead){
 	char input[256];
 	int sep = 0; //separator. 공백을 단위로 나누기 위함
 	char oper[8] = {0};
@@ -102,7 +184,8 @@ void base_print(Linklist* head, char path[256]){
 	char option[2][32] = {0};
 
 	print("20193058> "); //기본 프롬프트 모양
-	scanf("%s" &input);
+	fgets(input, sizeof(input), stdin); //fgets로 받는 것이 더 좋음
+	input[strlen(input) - 1] = '\0';
 	token = strtok(input, " ");
 
 	while(token != NULL){
@@ -156,6 +239,7 @@ void base_print(Linklist* head, char path[256]){
 int main(char argc, char *argv[]){
 	system("clear");
 	Linklist* head = NULL;
+	LogDetail* lhead = NULL;
 	Diclist* arr[32] = NULL;
 	int re = 0;
 	char path[256];
@@ -163,7 +247,7 @@ int main(char argc, char *argv[]){
 	mode_t dir_mode;
 	//FILE* logfile;
 
-	if(logfile = fopen("./logfile", "a+") == NULL){ //a+ or ra로 읽기도 동시에 할 수 있는지..
+	if(logfile = fopen("./logfile", "w+") == NULL){ //a+ or ra로 읽기도 동시에 할 수 있는지..
 		logfile = fopen("./logfile", "w+");
 	}
 
@@ -195,7 +279,7 @@ int main(char argc, char *argv[]){
 		return 0;
 	}
 	while(re){
-		base_print(head,path);
+		base_print(head, path, lhead);
 	}
 
 	return 0;
